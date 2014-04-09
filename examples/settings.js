58295ece9925d9156fc7d9cd3b6bd82b2134d1d7
Leore Avidar <leore.avidar@gmail.com>
2014-04-09 15:41:58 -0700

    closes #18, #19, and #20

--- examples/settings.js
+++ examples/settings.js
@@ -3,7 +3,7 @@ var LOB_API_KEY = NODE_ENV.LOB_API_KEY || 'YOUR_LOB_API_KEY';
 var LOB = new (require('../lib/main')) (LOB_API_KEY);
 
 /*
- * Settings Endpoint 
+ * Settings Endpoint
  */
 
 // List All settings
@@ -14,7 +14,7 @@ LOB.settings.list(function(err, res) {
 /**/
 
 // Retrieve a particular setting by SETTING_ID (required)
-/**/
+/**
 LOB.settings.get("100", function(err, res) {
 	console.log(err, res);
 });
