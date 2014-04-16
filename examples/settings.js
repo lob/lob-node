96feb7073e810f0d739a1b834ee9cddf8a799002
Leore Avidar <leore.avidar@gmail.com>
2014-04-16 12:14:10 -0700

    settings and services tests and examples

--- examples/settings.js
+++ examples/settings.js
@@ -6,15 +6,15 @@ Lob = new Lob('test_0dc8d51e0acffcb1880e0f19c79b2f5b0cc');
  */
 
 // List All settings
-/**
+//
 Lob.settings.list(function(err, res) {
 	console.log(err, res);
 });
 /**/
 
-// Retrieve a particular setting by SETTING_ID (required)
-/**
-Lob.settings.get("100", function(err, res) {
+// Retrieve a particular setting object
+//
+Lob.settings.get('100', function(err, res) {
 	console.log(err, res);
 });
 /**/
