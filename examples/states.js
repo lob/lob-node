7bdc0d341f11e5729b31d57f570b218c9ac3db11
Leore Avidar <leore.avidar@gmail.com>
2014-04-16 11:52:02 -0700

    States and Countries Tests + Examples

---
+++ examples/states.js
@@ -0,0 +1,16 @@
+var Lob = require('../lib/Lob');
+Lob = new Lob('test_0dc8d51e0acffcb1880e0f19c79b2f5b0cc');
+/* jshint camelcase: false */
+/*
+* States Endpoint
+*/
+
+
+// List All States with defaults
+//
+Lob.states.list(function(err, res) {
+  console.log(err, res);
+});
+
+
+/* jshint camelcase: false */
