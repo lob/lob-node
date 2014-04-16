be0f8f240ca185939f8ce78b72e5f80ef54a81cd
Leore Avidar <leore.avidar@gmail.com>
2014-04-16 12:48:09 -0700

    verification tests + examples

---
+++ examples/verification.js
@@ -0,0 +1,20 @@
+var Lob = require('../lib/lob');
+Lob = new Lob('test_0dc8d51e0acffcb1880e0f19c79b2f5b0cc');
+/* jshint camelcase: false */
+/*
+* Verify Endpoint
+*/
+
+// Verify and Address
+//
+Lob.verification.verify({
+  address_line1: '325 Berry Street',
+  address_line2: 'Unit 211',
+  address_city: 'San Francisco',
+  address_state: 'CA',
+  address_zip: '94158',
+  address_country: 'US',
+}, function(err, res) {
+  console.log(err, res);
+});
+/**/
