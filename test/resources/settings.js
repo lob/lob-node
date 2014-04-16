5eb2ccab81c989d308ec8c840cd6d48f3a5e59fe
Leore Avidar <leore.avidar@gmail.com>
2014-04-16 12:17:53 -0700

    restructred

--- test/resources/settings.js
+++ test/resources/settings.js
@@ -1,16 +1,17 @@
-var Lob = require('../lib/lob');
+var Lob = require('../../lib/lob');
 Lob = new Lob('test_0dc8d51e0acffcb1880e0f19c79b2f5b0cc');
 var Should;
 Should = require('should');
 /* jshint camelcase: false */
-describe('Countries', function() {
+describe('Settings', function() {
   describe('list', function() {
     it('should have correct defaults', function(done) {
-      Lob.countries.list(function(err, res) {
+      Lob.settings.list(function(err, res) {
         res.should.have.property('object');
         res.should.have.property('data');
         res.data.should.be.instanceof(Array);
-        return done();
+        res.object.should.eql('list');
+        done();
       });
     });
   });
--- test/resources/states.js
+++ test/resources/states.js
@@ -1,4 +1,4 @@
-var Lob = require('../lib/lob');
+var Lob = require('../../lib/lob');
 Lob = new Lob('test_0dc8d51e0acffcb1880e0f19c79b2f5b0cc');
 var Should;
 Should = require('should');
