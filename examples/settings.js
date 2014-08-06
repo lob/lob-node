566a2a2f9626141263ee1acc3aaec51069565638
Amrit Ayalur <a.ayalur@gmail.com>
2014-08-06 12:24:01 -0700

    Update example files

--- examples/settings.js
+++ examples/settings.js
@@ -1,5 +1,5 @@
-var Lob = require('../lib/lob');
-Lob = new Lob('test_0dc8d51e0acffcb1880e0f19c79b2f5b0cc');
+var lobFactory = require('../lib/index.js');
+var Lob = new lobFactory('test_0dc8d51e0acffcb1880e0f19c79b2f5b0cc');
 
 /*
  * Settings Endpoint
