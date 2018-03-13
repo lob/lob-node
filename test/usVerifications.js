84b9a44a0044eccc33bc8e1a839c54f9ae5cb067
Donald Little <dmlittle94@gmail.com>
2018-03-13 11:31:04 -0700

    feat(us-verifications): allow passing queries in verification creation (#197)

--- test/usVerifications.js
+++ test/usVerifications.js
@@ -6,12 +6,27 @@ describe('us_verifications', () => {
 
     it('verifies an address', (done) => {
       Lob.usVerifications.verify({
-        primary_line: '185 Berry St Ste 6600',
+        primary_line: 'deliverable',
         city: 'San Francisco',
         state: 'CA',
         zip_code: '94107'
       }, (err, res) => {
-        expect(res.primary_line).to.eql('185 BERRY ST STE 6600');
+        expect(res.primary_line).to.eql('1 TELEGRAPH HILL BLVD');
+        expect(res.deliverability).to.eql('deliverable');
+        return done();
+      });
+    });
+
+    it('verifies an address with custom case', (done) => {
+      Lob.usVerifications.verify({
+        primary_line: 'deliverable',
+        city: 'San Francisco',
+        state: 'CA',
+        zip_code: '94107'
+      }, {
+        case: 'proper'
+      }, (err, res) => {
+        expect(res.primary_line).to.eql('1 Telegraph Hill Blvd');
         expect(res.deliverability).to.eql('deliverable');
         return done();
       });
