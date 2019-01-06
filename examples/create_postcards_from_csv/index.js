70dddc2cd94fb7ed05469b96924b8de80dab24cb
Leore Avidar <leore@lob.com>
2019-01-06 13:23:22 -0800

    Add Metadata
    Adds metadata as part of the example

--- examples/create_postcards_from_csv/index.js
+++ examples/create_postcards_from_csv/index.js
@@ -42,6 +42,11 @@ parse(input, function (err, rows) {
         name: row[0],
         car: row[3],
         mileage: row[4]
+      },
+      metadata: {
+        name: row[0],
+        customer_id: "customer_123",
+        campaign: "Example Campaign"
       }
     }, function (err, postcard) {
       if (err) {
