4953c33bc4be8c5fe45bdb9029744d16640eab0f
Aditi <aditi@lob.com>
2019-12-17 15:40:44 -0800

    fix(intl-verifications): fixed failing test

--- test/intlVerifications.js
+++ test/intlVerifications.js
@@ -6,13 +6,11 @@ describe('intl_verifications', () => {
 
     it('verifies an address', (done) => {
       Lob.intlVerifications.verify({
-        primary_line: '370 Water St',
-        city: 'Summerside',
-        state: 'Prince Edward Island',
-        postal_code: 'C1N 1C4',
+        primary_line: 'deliverable',
         country: 'CA'
-      }, (err) => {
-        expect(err.status_code).to.eql(403);
+      }, (err, res) => {
+        expect(res.primary_line).to.eql('370 WATER ST');
+        expect(res.deliverability).to.eql('deliverable');
         return done();
       });
     });
