0b8cd0ebbfaae54ba697a3ff2c035da965ee4dc3
Julie Lin <julie.lin@lob.com>
2021-10-04 15:28:00 -0700

    Add card orders test

---
+++ test/cardOrders.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const Fs = require('fs');
+const file = Fs.readFileSync(`${__dirname}/assets/card_horizontal_2_125x3_375.pdf`);
+
+describe('cardOrders', () => {
+
+  describe('list', () => {
+
+    it('lists card orders given a card id', (done) => {
+      Lob.cards.create({
+        description: 'Test Card',
+        front: file,
+        back: file,
+        size: '2.125x3.375',
+      }, (err, res) => {
+        Lob.cardOrders.list(res.id, null, (err2, res2) => {
+          expect(res2.object).to.eql('list');
+          done();
+        });
+      });
+    });
+
+  });
+
+  describe('create', () => {
+
+    it('creates a card order', (done) => {
+      Lob.cards.create({
+        description: 'Test Card',
+        front: file,
+        back: file,
+        size: '2.125x3.375',
+      }, (err, res) => {
+        const cardOrderParams = {
+          'quantity_ordered': 10000
+        } 
+        Lob.cardOrders.create(res.id, cardOrderParams, (err2, res2) => {
+          expect(res.object).to.eql('card_order');
+          done();
+        });
+      });
+
+    });
+
+  });
+
+});
