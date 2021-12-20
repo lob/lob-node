fa75f2cf001273f6c7c73b3eb6b7823cbc38bd1e
BennyKitchell <bennykitchell@gmail.com>
2021-12-20 15:58:09 -0600

    chore: Remove unusable test

--- test/index.js
+++ test/index.js
@@ -1,4 +1,5 @@
 'use strict';
+const sinon = require('sinon');
 
 describe('Lob', () => {
 
@@ -31,18 +32,4 @@ describe('Lob', () => {
     expect(Lob.options.apiKey).to.eql(API_KEY);
     expect(Lob.options.host).to.eql('http://test');
   });
-
-  it('should propagate request errors', (done) => {
-    const options = { host: 'http://test' };
-    const Lob = require('../lib')(API_KEY, options);
-
-    Lob.addresses.list().then((res) => {
-      done();
-    })
-    .catch((err) => {
-      expect(err.message).to.match(/getaddrinfo (ENOTFOUND|EAI_AGAIN)/);
-      done();
-    });
-  });
-
 });
