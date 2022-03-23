1459a272c74674ac667a41318238a5ef017c8144
David Xu <david.xu@lob.com>
2022-03-23 13:29:17 -0700

    test fixes

--- test/usAutocompletions.js
+++ test/usAutocompletions.js
@@ -18,18 +18,18 @@ describe('us_autocompletions', () => {
       });
     });
 
-    it('returns a list of suggestions with custom case', (done) => {
-      Lob.usVerifications.verify({
-        primary_line: 'deliverable',
+    it.only('returns a list of suggestions with custom case', (done) => {
+      Lob.usAutocompletions.autocomplete({
+        address_prefix: '185 BER',
         city: 'San Francisco',
-        state: 'CA',
-        zip_code: '94107'
+        state: 'CA'
       }, {
         case: 'proper'
       }, (err, res) => {
         expect(err).to.not.exist;
         expect(res.suggestions).to.exist;
-        expect(res.suggestions[0].primary_line).to.eql('TEST KEYS DO NOT AUTOCOMPLETE US ADDRESSES');
+        expect(res.suggestions[0].primary_line).to.eql('Test Keys Do Not Autocomplete Us Addresses');
+
         return done();
       });
     });
