df8477ad8cabce4b4ef65ce3d51c709acec9c320
Shanna Murry <shanna.murry84@gmail.com>
2023-11-27 10:19:22 -0500

    linting

--- examples/create_postcard_intl.js
+++ examples/create_postcard_intl.js
@@ -1,28 +1,28 @@
-'use strict';
+"use strict";
 
 /*
  * Create an international address, then send a postcard with a custom PDF back.
  * Run me! This example works out of the box, "batteries included".
  */
 
-const fs = require('fs');
+const fs = require("fs");
 
-const lobFactory = require('../lib/index.js');
-const Lob = new lobFactory('YOUR_API_KEY');
+const lobFactory = require("../lib/index.js");
+const Lob = new lobFactory("YOUR_API_KEY");
 
 const file = fs.readFileSync(`${__dirname}/html/card.html`).toString();
 
 // Create the from address
 async function getFrom() {
   const from_address = await Lob.addresses.create({
-    name: 'Harry Zhang',
-    email: 'harry@lob.com',
-    phone: '5555555555',
-    address_line1: '2261 MARKET ST',
-    address_line2: '',
-    address_city: 'SAN FRANCISCO',
-    address_state: 'CA',
-    address_zip: '94114',
+    name: "Harry Zhang",
+    email: "harry@lob.com",
+    phone: "5555555555",
+    address_line1: "2261 MARKET ST",
+    address_line2: "",
+    address_city: "SAN FRANCISCO",
+    address_state: "CA",
+    address_zip: "94114",
   });
 
   return from_address.id;
@@ -32,16 +32,17 @@ async function createPostcard() {
   const from_id = await getFrom();
 
   // Create the address
-  Lob.addresses.create({
-      name: 'Harry Zhang',
-      email: 'harry@lob.com',
-      phone: '5555555555',
-      address_line1: '370 WATER ST',
-      address_line2: '',
-      address_city: 'SUMMERSIDE',
-      address_state: 'PRINCE EDWARD ISLAND',
-      address_zip: 'C1N 1C4',
-      address_country: 'CA',
+  Lob.addresses.create(
+    {
+      name: "Harry Zhang",
+      email: "harry@lob.com",
+      phone: "5555555555",
+      address_line1: "370 WATER ST",
+      address_line2: "",
+      address_city: "SUMMERSIDE",
+      address_state: "PRINCE EDWARD ISLAND",
+      address_zip: "C1N 1C4",
+      address_country: "CA",
     },
     (err, address) => {
       if (err) {
@@ -49,13 +50,13 @@ async function createPostcard() {
       } else {
         Lob.postcards.create(
           {
-            description: 'My Second Postcard',
+            description: "My Second Postcard",
             to: address.id,
             from: from_id,
             front: file,
             back: file,
             merge_variables: {
-              name: 'Robin',
+              name: "Robin",
             },
           },
           (err, postcard) => {
@@ -63,14 +64,15 @@ async function createPostcard() {
               console.log(err);
             } else {
               console.log(
-                'The Lob API responded with this postcard object: ',
+                "The Lob API responded with this postcard object: ",
                 postcard
               );
             }
           }
         );
       }
-    });
+    }
+  );
 }
 
 createPostcard();
